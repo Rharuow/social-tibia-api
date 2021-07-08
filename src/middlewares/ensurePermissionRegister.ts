5ce686d6fa745733e1185bbffdbb1541e45cd2cb
Harysson Soares <haryssonsoares@gmail.com>
2021-07-08 18:13:11 -0300

    wip: ensure permission register middleware

---
+++ src/middlewares/ensurePermissionRegister.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+
+export function ensurePermissionRegister(req: Request, res: Response,  next: NextFunction) {
+  try {
+  } catch(err) {
+  }
+}
