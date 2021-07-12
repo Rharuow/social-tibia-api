3adfe98bf0b33bda49d0d90445cfb395f518ab87
Harysson Soares <haryssonsoares@gmail.com>
2021-07-11 22:52:14 -0300

    feat: delete and list users

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,21 @@ import { Router } from "express";
 import { RootController } from "./controllers/RootController";
 import { CreateUserController } from './controllers/CreateUserController';
 import { ensurePermissionRegister } from "./middlewares/ensurePermissionRegister";
+import { DeleteUserController } from "./controllers/DeleteUserController";
+import { ListUserController } from "./controllers/ListUserController";
 
 const router = Router()
 
 const rootController = new RootController();
+
+//user routes
 const createUserController = new CreateUserController()
+const deleteUserController = new DeleteUserController()
+const listUserController = new ListUserController()
 
 router.get("/", rootController.run)
 router.post("/users", ensurePermissionRegister, createUserController.handle)
+router.get("/users", listUserController.handle)
+router.delete("/users", deleteUserController.handle)
 
 export { router }
