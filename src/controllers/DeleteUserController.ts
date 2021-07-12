6300bbfeb61600a1e4792d59203f513c1815f160
Harysson Soares <haryssonsoares@gmail.com>
2021-07-11 22:52:42 -0300

    feat: delete user controller

---
+++ src/controllers/DeleteUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { DeleteUserService } from "../servicies/User/DeleteuserService";
+
+
+export class DeleteUserController {
+    async handle (req: Request, res: Response) {
+        const deleteUserService = new DeleteUserService();
+
+        const {id} = req.body
+
+        try {
+            const user = deleteUserService.execute(id)
+            return res.json({
+                user,
+                message: 'User has been deleted'
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+}
