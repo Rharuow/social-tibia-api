be417748daff1fe8a6d907a951935d86b396a73b
Harysson Soares <haryssonsoares@gmail.com>
2021-07-12 18:39:15 -0300

    feat: create char service

--- src/servicies/Char/CreateCharService.ts
+++ src/servicies/Char/CreateCharService.ts
@@ -1,15 +1,20 @@
 
+import { getCustomRepository } from 'typeorm';
+
+import { CharRepositories } from '../../repositories/CharRepositories';
+import { UserRepositories } from '../../repositories/UserRepositories';
+
 interface ICharsRequest {
     name: string
     voc: string
     residence: string
     sex: string
     last_login: string
-    account_status: boolean
-    status: boolean
+    account_status: string
+    status: string
     level: number
-    max_level_shared: number
-    min_level_shared: number
+    max_lvl_shared: number
+    min_lvl_shared: number
     own: string
 }
 
@@ -23,9 +28,40 @@ export class CreateCharService {
         return voc.includes('Knight') ? 'ek' : voc.includes('Druid') ? 'ED' : voc.includes('Sorcerer') ? 'ms' : 'rp'
     }
 
-    async execute({name, voc, residence, sex, last_login, account_status, status, level, max_level_shared, min_level_shared, own}: ICharsRequest) {
+    statusPattern(status: string): boolean {
+        return status.includes("offline") ? false : true
+    }
+    
+    accountStatusPattern(accountStatus: string): boolean {
+        return accountStatus.includes("Free") ? false : true
+    }
 
-        
+    async execute({name, voc, residence, sex, last_login, account_status, status, level, max_lvl_shared, min_lvl_shared, own}: ICharsRequest) {
+        const charRepositories = getCustomRepository(CharRepositories)
+
+        const userRepositories = getCustomRepository(UserRepositories)
+
+        const userExists = await userRepositories.findOne(own)
+
+        if(!userExists) throw new Error("user doens't exists")
+
+        const char = charRepositories.create({
+            name,
+            voc: this.vocPattern(voc),
+            residence,
+            sex: this.sexPattern(sex),
+            last_login,
+            account_status: this.accountStatusPattern(account_status),
+            status: this.statusPattern(status),
+            level,
+            max_lvl_shared,
+            min_lvl_shared,
+            own
+        })
+
+        await charRepositories.save(char)
+
+        return char
 
     }
 }
