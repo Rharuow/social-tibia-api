870d30cb06cb23fc85e4169b0a8f223d2c8e5dc0
Harysson Soares <haryssonsoares@gmail.com>
2021-07-11 22:53:22 -0300

    feat: entity chars

---
+++ src/entities/Char.ts
@@ -0,0 +1,60 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
+
+
+@Entity('chars')
+export class Char {
+
+    @PrimaryColumn()
+    readonly id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    voc: string
+
+    @Column()
+    residence: string
+
+    @Column()
+    sex: string
+
+    @Column()
+    last_login: string
+
+    @Column()
+    account_status: boolean
+
+    @Column()
+    status: boolean
+
+    @Column()
+    level: number
+
+    @Column()
+    max_lvl_shared: number
+
+    @Column()
+    min_lvl_shared: number
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    @Column()
+    own: string
+
+    @JoinColumn({name: 'own'})
+    @ManyToOne(() => User)
+    ownChar: User 
+
+    constructor() {
+        if(!this.id) 
+            this.id = uuid()
+    }
+
+}
