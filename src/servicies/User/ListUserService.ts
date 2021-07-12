e8b8ba6faa01797c9855ef4a3fb96bc603605a59
Harysson Soares <haryssonsoares@gmail.com>
2021-07-11 22:54:43 -0300

    feat: list user service

---
+++ src/servicies/User/ListUserService.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../../repositories/UserRepositories";
+
+
+export class ListUserService {
+    async execute() {
+        const userRepository = getCustomRepository(UserRepositories)
+
+        const users = await userRepository.find()
+
+        return users
+    }
+}
