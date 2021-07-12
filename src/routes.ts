0308a3275378ab7bff2c7c1df93a4ce6b88ca57d
Harysson Soares <haryssonsoares@gmail.com>
2021-07-12 18:32:04 -0300

    feat: routes to create and list chars

--- src/routes.ts
+++ src/routes.ts
@@ -16,10 +16,21 @@ const rootController = new RootController();
 const createUserController = new CreateUserController()
 const deleteUserController = new DeleteUserController()
 const listUserController = new ListUserController()
+const listCharsByUserController = new ListCharsByUserController()
+
+//chars routes
+const createCharController = new CreateCharController()
+const listCharController = new ListCharController()
+
 
 router.get("/", rootController.run)
 router.post("/users", ensurePermissionRegister, createUserController.handle)
 router.get("/users", listUserController.handle)
 router.delete("/users", deleteUserController.handle)
+
+router.get("/users/:id/chars", listCharsByUserController.handle)
+
+router.get("/chars", listCharController.handle)
+router.post("/chars", createCharController.handle)
 
 export { router }
