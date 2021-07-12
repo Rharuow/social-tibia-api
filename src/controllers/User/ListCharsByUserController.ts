8564af038f441636895b410cae2208a0fd4ea66d
Harysson Soares <haryssonsoares@gmail.com>
2021-07-12 18:34:07 -0300

    feat: list chars by user controller

---
+++ src/controllers/User/ListCharsByUserController.ts
@@ -0,0 +1,17 @@
+
+import { Request, Response } from 'express';
+import { ListCharsByUserService } from '../../servicies/User/ListCharsByUserService';
+
+export class ListCharsByUserController {
+
+  async handle(req: Request, res: Response) {
+    const listCharsByUserService = new ListCharsByUserService()
+
+    const {id} = req.params
+
+    const chars = listCharsByUserService.execute(id)
+
+    return res.json(listCharsByUserService)
+
+  }
+}
