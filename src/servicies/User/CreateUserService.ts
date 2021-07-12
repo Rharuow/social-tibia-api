de9324e28ab85a343c866e36af9c3576ef8a4245
Harysson Soares <haryssonsoares@gmail.com>
2021-07-11 22:54:17 -0300

    feat: create user service

---
+++ src/servicies/User/CreateUserService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm";
+import { hash } from 'bcryptjs'
+
+import { UserRepositories } from '../../repositories/UserRepositories';
+
+
+interface IUserRequest {
+  username: string
+  email: string
+  password: string
+}
+
+export class CreateUserService {
+  
+  async execute({username, email, password}: IUserRequest) {
+    const userRepository = getCustomRepository(UserRepositories)
+
+    const userAlreadyExists = await userRepository.findOne({email})
+
+    if(userAlreadyExists) throw new Error("User already exists")
+    
+    console.log(password)
+    const passwordHashed = await hash(password, parseInt(process.env.HASH_SALTS))
+
+    const user = userRepository.create({username, email, password: passwordHashed})
+
+    await userRepository.save(user)
+
+    return user
+
+  }
+}
