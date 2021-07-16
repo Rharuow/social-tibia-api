ea6694fd15d2980e77d6e13f2ac8b9cb4aea4b91
Harysson Soares <haryssonsoares@gmail.com>
2021-07-16 17:42:03 -0300

    feat: create requirments service

--- src/servicies/Requiriment/CreateRequirimentService.ts
+++ src/servicies/Requiriment/CreateRequirimentService.ts
@@ -1,14 +1,27 @@
 import { getCustomRepository } from "typeorm";
 import { RequirimentRepositories } from "../../repositories/RequirimentRepositories";
+import { CharRepositories } from '../../repositories/CharRepositories';
 
 
 export class CreateRequirimentService {
+
     async execute(charId: string) {
-        const requirmentRepository = getCustomRepository(RequirimentRepositories)
 
-        const requiriment = requirmentRepository.create({charId})
+        if(!charId) throw new Error("Add some char to create requiriments")
 
-        
+        const requirimentRepository = getCustomRepository(RequirimentRepositories)
+
+        const charRepositories = getCustomRepository(CharRepositories)
+
+        const char = await charRepositories.findOne(charId)
+            
+        if(!char) throw new Error("Char doesn't exists")
+
+        const requiriment = requirimentRepository.create({charId})
+
+        await requirimentRepository.save(requiriment)
+
+        return requiriment
 
     }
 }
